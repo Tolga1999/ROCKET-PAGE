efa1245a7816c8443ac3f8f1f148751863f5e9c2
Tjips <thomas@scheepers.com>
2022-09-12 19:13:19 +0100

    Beetje testen

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,6 @@
-console.log("het werkt")
+const scrollContainer = document.querySelector("main");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+});
