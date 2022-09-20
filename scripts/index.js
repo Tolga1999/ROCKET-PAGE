82f53aa256bb5a296e961914282c6b6dacc0889a
Tolga1999 <tolgadoganer@outlook.com>
2022-09-20 22:04:36 +0200

    knop werkt, gallery opent
    het werkt allemaal, alleen is wel de alignment veranderd voor een of andere redenn

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,15 @@
-function menuFunction(){
-    let gallery = document.getElementById("gallery")
-    if( gallery.style.display !== "none"){
-        gallery.style.display = "none"
+let openGallery = new Boolean(false);
+function menuFunction() {
+    gallery = document.querySelector('.gallery')
+    galleryOpenButton = document.getElementById('galleryOpenButton')
+    // gives true or false as value
+    if(openGallery == false){
+        gallery.classList.add('galleryTop')
+        openGallery = true
+        galleryOpenButton.style.display = "none"
     }else{
-        gallery.style.display = "flex"
+        gallery.classList.remove('galleryTop')
+        openGallery = false
+        galleryOpenButton.style.display = "inline"
     }
 }
