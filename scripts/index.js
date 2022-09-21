87a62cbfa80afb9cd9bde438438cdd03ac4ff682
Tolga1999 <tolgadoganer@outlook.com>
2022-09-21 10:23:26 +0200

    GALLERIJ button changed

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,17 @@ let openGallery = new Boolean(false);
 function menuFunction() {
     gallery = document.querySelector('.gallery')
     galleryOpenButton = document.getElementById('galleryOpenButton')
+    galleryCloseButton = document.getElementById('galleryCloseButton')
     // gives true or false as value
     if(openGallery == false){
         gallery.classList.add('galleryTop')
         openGallery = true
-        galleryOpenButton.style.display = "none"
+        galleryCloseButton.style.display = "inline"
     }else{
         gallery.classList.remove('galleryTop')
         openGallery = false
         galleryOpenButton.style.display = "inline"
+        galleryCloseButton.style.display = "none"
     }
 }
 
